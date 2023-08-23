e42f283e69d4fae838129424fc56aea72a49f76c
Me <nozimmansurov@Nozims-MacBook-Air.local>
2023-08-23 16:34:18 +0500

    cv changed

--- src/constants/lists/index.ts
+++ src/constants/lists/index.ts
@@ -21,15 +21,77 @@ const summaries = [
   },
   {
     id: 6,
-    item: "Experience working with a collaboration team on GitHub and GIT",
+    item: "Experience working with a collaboration team on GitHub and GitLab",
   },
 ];
 
 const experiences = [
   {
-    id: 1,
+    id: 2,
+    title: "Frontend developer, AbuTech",
+    time: "Feb 2023 - Aug 2023",
+    lists: [
+      {
+        id: 1,
+        item: "Collaborated with Backend developers on the development of Admin panels, Landing pages and Online platforms",
+      },
+      {
+        id: 2,
+        item: "Assigned and completed tasks using project management tools such as Jira and ClickUp",
+      },
+      {
+        id: 3,
+        item: "Ensure timely completion of assigned tasks and deliver high-quality frontend solutions",
+      },
+      {
+        id: 4,
+        item: "Built cross-browser compatible and accessibility compliant websites, resulting in 22% faster load time than industry average"
+      },
+      {
+        id: 5,
+        item: "Produced multiple visual elements of web applications by translating UI/UX design wireframes into code, producing high quality markup using HTML5 and CSS3"
+      },
+      {
+        id: 6,
+        item: "Worked closely with an agile team of 10 members and provided end-to-end solutions for clients",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Frontend developer, CodeTech",
+    time: "Sep 2022 - Feb 2023",
+    lists: [
+      {
+        id: 1,
+        item: "Collaborated with Backend Engineers to develop REST API contracts",
+      },
+      {
+        id: 2,
+        item: "Organizing webinars with responsibilities: share and evaluate work tasks, oversee the project process and encourage areas for growth and improvement",
+      },
+      {
+        id: 3,
+        item: "Worked with UX/UI design teams to improve existing applications and increase conversion rates by 20%",
+      },
+      {
+        id: 4,
+        item: "Designed multiple dynamic and browser compatible pages using HTML5, CSS3, Javascript (React, Redux)",
+      },
+      {
+        id: 5,
+        item: "Agile project methodology was used to allocate project time correctly and complete it on time",
+      },
+      {
+        id: 6,
+        item: "The project was led by a Senior Engineer attached to Git and GitHub and it was peer-reviewed",
+      },
+    ],
+  },
+  {
+    id: 3,
     title: "Mentor assistant, Najot Ta'lim",
-    time: "June 2022 - Dec 2022",
+    time: "May 2022 - Nov 2022",
     lists: [
       {
         id: 1,
@@ -53,54 +115,6 @@ const experiences = [
       },
     ],
   },
-  {
-    id: 2,
-    title: "Frontend developer, CodeTech",
-    time: "Dec 2022 - June 2023",
-    lists: [
-      {
-        id: 1,
-        item: "Collaborated with Backend Engineer to develop REST API contracts",
-      },
-      {
-        id: 2,
-        item: "Organizing webinars with responsibilities: share and evaluate work tasks, oversee the project process and encourage areas for growth and improvement",
-      },
-      {
-        id: 3,
-        item: "Agile project methodology was used to allocate project time correctly and complete it on time. The project was led by a Senior Engineer attached to Git and GitHub and it was peer-reviewed",
-      },
-    ],
-  },
 ];
 
-const projects = [
-  {
-    id: 1,
-    title: "EducationBox",
-    lists: [
-      {
-        id: 1,
-        item: "Trained group students includes 20-30 members in using Full Stack Web Development course",
-      },
-      {
-        id: 2,
-        item: " Providing students with an hour of practice after the lesson on topics covered",
-      },
-      {
-        id: 3,
-        item: "At the end of each month, take tests from students according to the criteria and provide feedback on their results",
-      },
-      {
-        id: 4,
-        item: "Individual work with students who have difficulties learning a lesson",
-      },
-      {
-        id: 5,
-        item: "Discussed and solved about 100 simple and medium tasks from Codewars",
-      },
-    ],
-  },
-];
-
-export { summaries, projects, experiences };
+export { summaries, experiences };
