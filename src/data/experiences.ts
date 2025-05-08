e25a59159be29319ef447ee8089a9bd8999a55c7
liildev <mansurov.jr@mail.ru>
2025-05-08 14:32:39 +0500

    fix expreiences

--- src/data/experiences.ts
+++ src/data/experiences.ts
@@ -15,19 +15,11 @@ export const experiences = [
     location: "Tashkent, Chilanzar",
   },
   {
-    jobTitle: "React Developer",
-    company: "Codetech",
-    companyUrl: "https://codetech.io",
-    startDate: "2022-09-01",
-    endDate: "2023-02-01",
-    location: "Tashkent, Chilanzar",
-  },
-  {
     jobTitle: "Mentor Assistant",
     company: "Najot Ta'lim",
     companyUrl: "https://najottalim.uz",
-    startDate: "2022-05-01",
-    endDate: "2022-11-01",
+    startDate: "2022-08-01",
+    endDate: "2022-02-01",
     location: "Tashkent, Chilanzar",
   },
 ];
