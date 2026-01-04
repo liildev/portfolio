1f175044da9838ebc8bb472d3d70d3871e46a3ce
Nozim Mansurov <liildev@mail.ru>
2026-01-04 05:50:35 +0500

    added: new images

--- lib/meta.ts
+++ lib/meta.ts
@@ -5,25 +5,25 @@ export const pagesMeta = {
     title: 'Liil Dev',
     description:
       'Frontend engineer focused on performance, precision, and pixel perfection.',
-    image: 'me.png',
+    image: 'me.webp',
   },
   projects: {
     title: 'Projects',
     description: `I'm obsessed with side projects and building in public. Here you can explore a collection of websites I've built over time – some still actively growing, others that served their purpose before sunsetting. Each one represents a unique learning journey.`,
-    image: 'projects.jpg',
+    image: 'projects/main.webp',
     path: ROUTES.projects,
   },
   about: {
     title: 'About',
     description:
       'The path that brought me here - roles, challenges, and growth.',
-    image: 'about.jpg',
+    image: 'about.webp',
     path: ROUTES.about,
   },
   reminder: {
     title: 'Reminder',
     description: 'Fall down seven times stand up eight.',
-    image: 'reminder.jpg',
+    image: 'proverb/main.webp',
     path: ROUTES.reminder,
   },
 };
